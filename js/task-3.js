836a08beb56f8fdb06b94f196e77f3cf5a16aadc
Oleksandr <114010794+Leximuzz@users.noreply.github.com>
2024-04-22 23:45:53 +0300

    HW_M3

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+
+    for (let i = 0; i <= numbers; i++){
+        if (i > value) {
+            maxArr = maxArr.push(numbers[i]);
+            console.log(numbers[i])
+      } 
+    }
+    return ;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
